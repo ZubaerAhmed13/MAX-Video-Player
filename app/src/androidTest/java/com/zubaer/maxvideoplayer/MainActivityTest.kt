85db62b7a55ae594c10e0aa2ccdac61bc8113a5c
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-06 19:51:49 +0200

    fix: compile API-35 instrumentation tests

--- app/src/androidTest/java/com/zubaer/maxvideoplayer/MainActivityTest.kt
+++ app/src/androidTest/java/com/zubaer/maxvideoplayer/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package com.zubaer.maxvideoplayer
 
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
