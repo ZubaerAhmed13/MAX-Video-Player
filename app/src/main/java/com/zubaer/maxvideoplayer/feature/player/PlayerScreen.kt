b713b4a43a5f3a8262bc7e02d3149b6e10f3d2ca
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-06 13:01:19 +0200

    fix: use AndroidX vararg opt-in syntax

--- app/src/main/java/com/zubaer/maxvideoplayer/feature/player/PlayerScreen.kt
+++ app/src/main/java/com/zubaer/maxvideoplayer/feature/player/PlayerScreen.kt
@@ -39,7 +39,7 @@ import com.zubaer.maxvideoplayer.core.model.PlaybackError
 import com.zubaer.maxvideoplayer.playback.session.PlaybackConnection
 import kotlin.math.roundToLong
 
-@androidx.annotation.OptIn(markerClass = UnstableApi::class)
+@androidx.annotation.OptIn(markerClass = [UnstableApi::class])
 @Composable
 fun PlayerScreen(
     media: AppMedia,
