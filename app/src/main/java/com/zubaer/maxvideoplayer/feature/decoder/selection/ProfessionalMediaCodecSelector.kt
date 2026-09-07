604e6fb86e72aebed674e5ed7cd2d24afd39db6f
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-08 01:11:33 +0200

    Step 6: opt in Media3 codec selector at file scope

--- app/src/main/java/com/zubaer/maxvideoplayer/feature/decoder/selection/ProfessionalMediaCodecSelector.kt
+++ app/src/main/java/com/zubaer/maxvideoplayer/feature/decoder/selection/ProfessionalMediaCodecSelector.kt
@@ -1,3 +1,5 @@
+@file:androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+
 package com.zubaer.maxvideoplayer.feature.decoder.selection
 
 import androidx.media3.exoplayer.mediacodec.MediaCodecInfo
